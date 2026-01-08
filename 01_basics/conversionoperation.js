8c3507427cc20d45bb5713a3920dfb700498deaa
Akshay Vaidhya <akshayjain7828@gmail.com>
2026-01-08 03:51:24 +0000

    basics

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,12 @@
+let score = "33" ;
+console.log(typeof score);
+
+let conversion = Number(score) ;
+console.log(typeof conversion);
+
+let bool=0
+let islogged=Boolean(bool) ;
+
+console.log(islogged);
+console.log(2**4);
+
